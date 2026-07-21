a8717396b3a475a73d189bf279c0b0fca3b574ce
Andrew <13841202+andrew01234567890@users.noreply.github.com>
2026-07-21 11:55:22 +0100

    feat(orch): prepare activation publisher

--- crates/pgshard-orch/src/lib.rs
+++ crates/pgshard-orch/src/lib.rs
@@ -7,6 +7,7 @@ pub(crate) mod agent_status;
 mod boottime;
 mod catalog_activation_challenge;
 mod catalog_activation_live_objects;
+mod catalog_activation_publisher;
 pub mod catalog_candidate;
 mod catalog_materialization;
 pub mod config;
