85f8f112a4034753731c711e3795cb58e6663f09
andrew01234567890 <13841202+andrew01234567890@users.noreply.github.com>
2026-08-18 22:28:27 +0100

    operator: use the shared pointer helper in the admin reconciler

--- internal/operator/admin.go
+++ internal/operator/admin.go
@@ -2,7 +2,6 @@ package operator
 
 import (
 	"context"
-	"k8s.io/utils/ptr"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -10,6 +9,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	pgshardv1alpha1 "github.com/andrew01234567890/pgshard/api/v1alpha1"
